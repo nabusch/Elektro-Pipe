872198f28d4ffdf76288f84157e3d163afebaaa6
wanjam <moessing@wwu.de>
2017-02-13 14:33:22 +0100

    parallel prep03runICA

--- prep03_runICA.m
+++ prep03_runICA.m
@@ -1,4 +1,4 @@
-function EEG = prep03_runICA(EP)
+function prep03_runICA(EP)
 
 [cfg_dir, cfg_name, ~] = fileparts(EP.cfg_file);
 [sub_dir, sub_name, ~] = fileparts(EP.cfg_file);
@@ -10,20 +10,23 @@ EP.S = readtable(EP.st_file);
 
 who_idx = get_subjects(EP);
 
-
-for isub = 1:length(who_idx)
+for isub=1:length(who_idx)
+    % Load CFG file. I know, eval is evil, but this way we allow the user
+    % to give the CFG function any arbitrary name, as defined in the EP
+    % struct.
+    evalstring = ['ALLCFG{',num2str(isub),'} = ' cfg_name '(' num2str(who_idx(isub)) ', EP.S);'];
+    eval(evalstring);
+end
+    
+parfor isub = 1:length(who_idx)
     
     EEG = [];
-    
+    CFG = ALLCFG{isub};
     % --------------------------------------------------------------
     % Prepare data.
     % --------------------------------------------------------------
     
-    % Load CFG file. I know, eval is evil, but this way we allow the user
-    % to give the CFG function any arbitrary name, as defined in the EP
-    % struct.
-    evalstring = ['CFG = ' cfg_name '(' num2str(who_idx(isub)) ', EP.S);'];
-    eval(evalstring);
+
     
     % Write a status message to the command line.
     fprintf('\nNow processing subject %s, (number %d of %d).\n\n', ...
@@ -94,10 +97,9 @@ for isub = 1:length(who_idx)
     % --------------------------------------------------------------
     EEG = pop_editset(EEG, 'setname', [CFG.subject_name '_ICA.set']);
     EEG = pop_saveset( EEG, [CFG.subject_name '_ICA.set'] , CFG.dir_eeg);
-        
-    EP.S.has_ICA(who_idx(isub)) = 1;
-    writetable(EP.S, EP.st_file)
-
 end
+
+    EP.S.has_ICA(who_idx) = 1;
+    writetable(EP.S, EP.st_file)
 
 fprintf('Done.\n')
