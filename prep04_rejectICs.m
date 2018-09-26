90661934d1ca3b36387917a62f7bc2872a650d8b
Wanja Mössing <wanjam@mailbox.org>
2018-09-26 15:54:26 +0200

    Combine eyetrackerica and SASICA
    - enables a combination of eyetracking based occular IC detection and rejection and a subsequent search for other artifacts via SASICA

--- prep04_rejectICs.m
+++ prep04_rejectICs.m
@@ -17,16 +17,16 @@ for isub = 1:length(who_idx)
     % --------------------------------------------------------------
     % Prepare data.
     % --------------------------------------------------------------
-
+    
     % Load CFG file. I know, eval is evil, but this way we allow the user
     % to give the CFG function any arbitrary name, as defined in the EP
     % struct.
     evalstring = ['CFG = ' cfg_name '(' num2str(who_idx(isub)) ', EP.S);'];
     eval(evalstring);
-        
+    
     % Write a status message to the command line.
     fprintf('\nNow working on subject %s, (number %d of %d to process).\n\n', ...
-        CFG.subject_name, isub, length(who_idx));   
+        CFG.subject_name, isub, length(who_idx));
     
     % Load data set.
     if CFG.keep_continuous && strcmp(CFG.ica_rm_continuous, 'cont')
@@ -45,34 +45,28 @@ for isub = 1:length(who_idx)
     if CFG.eye_ica && ~CFG.eyetracker_ica
         error(['cfg.eye_ica is deprecated. Use CFG.eyetracker_ica ',...
             'instead.']);
-%         if CFG.eye_ica_useEP
-%             if EP.S.Tracker_ICA(isub)
-%                 EEG         = create_blink_channel(EEG);
-%                 fprintf(['You chose to use eyetracking data to select ICA-\n'...
-%                     'components. To do so, use ''Eyegaze_X'' and ''Eyegaze_Y''\n'...
-%                     'instead of HEOG and VEOG.\nSelect ''correlation with'...
-%                     'other channel(s)'' and insert ''Blinks''...\n']);
-%             else
-%                 fprintf(2,['NOTE: According to the Excel table, this subject has\n'...
-%                     'noisy Eyetracking data. Please use ''HEOG'' and ''VEOG''\n'...
-%                     'instead of Eyetracking channels. Make sure to unselect\n'...
-%                     'the Blinkchannel if it''s entered, as it has not been\n'...
-%                     'created for the present subject!\n']);
-%             end
-%         else
-%             EEG         = create_blink_channel(EEG);
-%             fprintf(['You chose to use eyetracking data to select ICA-\n'...
-%                 'components. To do so, use ''Eyegaze_X'' and ''Eyegaze_Y''\n'...
-%                 'instead of HEOG and VEOG.\nSelect ''correlation with'...
-%                 'other channel(s)'' and insert ''Blinks''...\n']);
-%         end
+        %         if CFG.eye_ica_useEP
+        %             if EP.S.Tracker_ICA(isub)
+        %                 EEG         = create_blink_channel(EEG);
+        %                 fprintf(['You chose to use eyetracking data to select ICA-\n'...
+        %                     'components. To do so, use ''Eyegaze_X'' and ''Eyegaze_Y''\n'...
+        %                     'instead of HEOG and VEOG.\nSelect ''correlation with'...
+        %                     'other channel(s)'' and insert ''Blinks''...\n']);
+        %             else
+        %                 fprintf(2,['NOTE: According to the Excel table, this subject has\n'...
+        %                     'noisy Eyetracking data. Please use ''HEOG'' and ''VEOG''\n'...
+        %                     'instead of Eyetracking channels. Make sure to unselect\n'...
+        %                     'the Blinkchannel if it''s entered, as it has not been\n'...
+        %                     'created for the present subject!\n']);
+        %             end
+        %         else
+        %             EEG         = create_blink_channel(EEG);
+        %             fprintf(['You chose to use eyetracking data to select ICA-\n'...
+        %                 'components. To do so, use ''Eyegaze_X'' and ''Eyegaze_Y''\n'...
+        %                 'instead of HEOG and VEOG.\nSelect ''correlation with'...
+        %                 'other channel(s)'' and insert ''Blinks''...\n']);
+        %         end
     end
-%     [EEG, com] = SASICA(EEG);
-    %%
-    keyboard;
-    EEG = evalin('base','EEG'); %SASICA stores the results in base workspace via assignin. So we have to use this workaround...
-    EEG = eegh(com,EEG);
-
     
     %% Mark Eyetracking based occular artifacts
     if CFG.eyetracker_ica
@@ -90,13 +84,23 @@ for isub = 1:length(who_idx)
         
         % Auto-flag ocular ICs based on sac/fix variance ratio
         [EEG, vartable] = pop_eyetrackerica(EEG, types{sacdx},...
-            types{fixdx}, [5 0], CFG.eyetracker_ica_varthresh, 2, 1, 4);
+            types{fixdx}, [5 0], CFG.eyetracker_ica_varthresh, 2,...
+            CFG.eyetracker_ica_feedback ~= 4, CFG.eyetracker_ica_feedback);
+        
+        if CFG.eyetracker_ica_feedback ~= 4
+            fprintf(2, 'Hit continue or F5 to proceed!\n');
+            keyboard; % wait for user to check eyetrackerica output
+        end
+        %% Subtract the components identified via eyetrackerica
+        [EEG, com] = pop_subcomp(EEG, find(EEG.reject.gcompreject),1);
+        EEG = eegh(com,EEG);
     end
-    %%
-    [EEG, com] = pop_subcomp(EEG, find(EEG.reject.gcompreject),1);
-    if isempty(com)
-        return
-    end
+    
+    %% run SASICA on the remaining components to identify and mark EMG/EKG etc
+    [EEG, com] = SASICA(EEG);
+    fprintf(2, 'Hit continue or F5 to proceed!\n')
+    keyboard; % wait for user to fiddle around with SASICA
+    EEG = evalin('base','EEG'); % SASICA stores the results in base workspace via assignin. So we have to use this workaround...
     EEG = eegh(com,EEG);
     
     % --------------------------------------------------------------
@@ -113,22 +117,4 @@ for isub = 1:length(who_idx)
     close all;
 end
 
-fprintf('Done.\n')   
-
-% %%
-% eeg_SASICA(EEG)
-% %%
-%  [EEG, com] = SASICA(EEG, ...        
-%         'EOGcorr_enable',1,...
-%         'EOGcorr_Heogchannames',70,...
-%         'EOGcorr_corthreshH','auto 4',...
-%         'EOGcorr_Veogchannames',71,...
-%         'EOGcorr_corthreshV','auto 4',...
-%         'focalcomp_enable',1,...
-%         'focalcomp_focalICAout','auto',...
-%         'autocorr_enable',1,...
-%         'autocorr_autocorrint',20,...
-%         'autocorr_dropautocorr','auto',...
-%         'opts_noplot',0,...
-%         'opts_nocompute',0,...
-%         'opts_FontSize',14);
+fprintf('Done.\n')
