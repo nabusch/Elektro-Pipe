f73115c5ca8504114875d787fe1984ee80f18b1c
Wanja Mössing <wanjam@mailbox.org>
2019-02-11 16:44:22 +0100

    Introduce FFT design
    - adds design_runFFT.m & additions in design_master

--- private/Sample files/design_master.m
+++ private/Sample files/design_master.m
@@ -15,7 +15,7 @@ EP.EEG_Analysis_Folder = ''; % the subfolder containing code for the current pro
 % REQUIRED INPUTS:
 
 % Load the subjects table.
-addpath(fullfile([EP.dir_experiment filesep EP.EEG_Analysis_Folder]))
+addpath(fullfile([EP.dir_experiment filesep EP.EEG_Analysis_Folder]));
 EP.S = readtable('.xlsx'); %name of SubjectTable Excel-sheet.
 
 % Summary name for this project. Determines name of output folder.
@@ -23,13 +23,13 @@ EP.project_name = ''; %e.g., Grand_ERP_encoding or ER-TFA ...
 
 % Load the design file. The "D" structure contains information on the
 % factors and factor levels of the design.
-addpath(fullfile([EP.dir_experiment filesep EP.EEG_Analysis_Folder]))
+addpath(fullfile([EP.dir_experiment filesep EP.EEG_Analysis_Folder]));
 EP.D = get_design;
 
 % Define the configuration file as a string. It does not make sense to
 % actually load the cfg file with getcfg here, because we later need the
 % subject-specific cfg file that includes file paths for this each subject.
-addpath(fullfile([EP.dir_experiment filesep EP.EEG_Analysis_Folder]))
+addpath(fullfile([EP.dir_experiment filesep EP.EEG_Analysis_Folder]));
 EP.cfgfile = which('get_cfg');
 
 % While the cfg file defines where the EEG data are located, there are
@@ -42,16 +42,17 @@ EP.filename_in = '_ICArej';
 % the cfg file (e.g. by pointing to cfg.dir_eeg) because we do not know
 % which directory we will want to use. It could be cfg.eeg or cfg.fft or
 % cfg.eeg_hilbert etc. So we must define it here.
-EP.dir_in = [EP.dir_experiment filesep 'EEG'];
+EP.dir_in = [EP.dir_experiment, filesep, 'EEG'];
 
 % Folder where to store the results. Elektropipe will make separate
 % subfolders in dir_out, one for each design.
-EP.dir_out = [EP.dir_experiment, filesep, 'EEG-Results',filesep,EP.project_name];
+EP.dir_out = [EP.dir_experiment, filesep, 'EEG-Results', filesep,...
+    EP.project_name];
 
 % What exactly do you want to do?
 % - erp: grand average ERP.
 % - tf:  Subject-wise wavelet analysis.
-% - for the future: other prcesses like fft or tf analysis.
+% - fft: Subject-wise fft analysis.
 EP.process = ''; 
 
 % ----------------------------------------------------------------------
@@ -79,8 +80,9 @@ EP.keepdouble = 0;
 % one file per subject with the single-trial data. Average data are
 % unaffected by this setting.
 EP.singletrialTF = 0;
+EP.singletrialFFT = 1; 
 
-%% Run grandaverage for all designs and subejcts.
+%% Run process for all designs and subejcts.
 % Required input:
 % EP: struct containing parameters for the computation. See above.
 switch EP.process
@@ -88,4 +90,10 @@ switch EP.process
         design_erp_grandaverage(EP);
     case 'tf'
         design_runtf(EP);
+    case 'fft'
+        tic;
+        design_runFFT(EP);
+        toc;
 end
+elektro_notify('moessing@wwu.de', [EP.process, ' done!']);
+disp('done.')
